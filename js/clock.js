afc29e9a6d55f851a0fca8c7ee884c31264b12e2
Suhyeon Lee <114565244+Lesh97@users.noreply.github.com>
2022-11-04 15:03:57 +0900

    2022-11-04 Lesh add update src url

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,4 @@
-const clock = document.querySelector("h2 #clock");
+const clock = document.querySelector("h2#clock");
 
 nowTime = () => {
     const date = new Date();
@@ -6,7 +6,7 @@ nowTime = () => {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
 
-    clock.innerText = `${hours} : ${minutes} : ${seconds}`
+    clock.innerText = `${hours} : ${minutes} : ${seconds}`;
 };
 
 nowTime();
